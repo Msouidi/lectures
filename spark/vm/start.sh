eaa0b6c6f102f435c042ade96136ebdeea434a8f
Mohamed Souidi <mohamed.souidi@eulerhermes.com>
2022-03-24 14:39:00 +0100

    Update spark start

--- spark/vm/start.sh
+++ spark/vm/start.sh
@@ -6,6 +6,7 @@ echo "[$(DATE)] [Info] [Spark] Starting spark..."
 export  SPARK_HOME=/usr/local/spark; \
 export PATH=$SPARK_HOME/bin:$PATH; \
 export PYSPARK_DRIVER_PYTHON=jupyter; \
-export PYSPARK_DRIVER_PYTHON_OPTS='notebook --ip=0.0.0.0 --allow-root'; \
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password=''"; \
 export PATH=$PATH:~/.local/bin; \
-pyspark
+pyspark &
+echo "[$(DATE)] [Info] [Spark] Runing spark http://192.168.30.12:8888"
