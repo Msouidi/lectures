1bb20de972ec862469b42844deee8102abc6d8c4
Mohamed Souidi <mohamed.souidi@eulerhermes.com>
2023-05-09 12:18:03 +0200

    update spark

--- spark/vm/start.sh
+++ spark/vm/start.sh
@@ -1,12 +1,8 @@
-DATE() {
-  date '+%Y-%m-%d %H:%M:%S'
-}
-
-echo "[$(DATE)] [Info] [Spark] Starting spark..."
+echo "[Info] [Spark] Starting spark..."
 export  SPARK_HOME=/usr/local/spark; \
 export PATH=$SPARK_HOME/bin:$PATH; \
 export PYSPARK_DRIVER_PYTHON=jupyter; \
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=0.0.0.0 --allow-root --NotebookApp.token='' --NotebookApp.password=''"; \
 export PATH=$PATH:~/.local/bin; \
 pyspark &
-echo "[$(DATE)] [Info] [Spark] Runing spark http://192.168.30.12:8888"
+echo "[Info] [Spark] Runing spark http://192.168.33.12:8888"
